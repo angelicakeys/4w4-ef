aca2758e5c9e776b32487a2ea3d92b2b72a37860
angelicakeys <75345767+angelicakeys@users.noreply.github.com>
2021-05-18 09:44:54 -0400

    etape1

---
+++  angelica-montufar-4w4-ef/category-cours.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+ 
+get_header();
+?>
+
+//category cours
+	<main id="primary" class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+	
+				<?php
+				// echo (is_category('Projets')? '### oui projets ###': '### non ###');
+				// the_archive_title( '<h1 class="page-title">', '</h1>' );
+				echo '<h1 class="page-title">' . single_cat_title( '', false ) . '</h1>';
+				the_archive_description( '<div class="archive-description">', '</div>' );
+		
+				?>	
+			
+			</header><!-- .page-header -->
+			<section class="galerie">
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				convertirTableau($tPropriété);
+				get_template_part( 'template-parts/content', 'gabarit' );
+
+			endwhile;?>
+
+			</section>
+
+		<?php endif; ?>
+		?>
+
+	</main><!-- #main -->
+
+
+
+
+
+
+<?php
+
+
+
+// get_sidebar();
+get_footer();
+
+function convertirTableau(&$tPropriété)
+{
+	/*
+	$titre = get_the_title(); 
+	// 582-1W1 Mise en page Web (75h)
+	$sigle = substr($titre, 0, 7);
+	$nbHeure = substr($titre,-4,3);
+	$titrePartiel =substr($titre,8,-6);
+	$session = substr($titre, 4,1);
+	// $contenu = get_the_content();
+	// $resume = substr($contenu, 0, 200);
+	$typeCours = get_field('type_de_cours');
+*/
+
+	$tPropriété['titre'] = get_the_title(); 
+	$tPropriété['sigle'] = substr($tPropriété['titre'], 0, 7);
+	$tPropriété['nbHeure'] = substr($tPropriété['titre'],-6,6);
+	$tPropriété['titrePartiel'] = substr($tPropriété['titre'],8,-6);
+	$tPropriété['session'] = substr($tPropriété['titre'], 4,1);
+	$tPropriété['typeCours'] = get_field('type_de_cours');
+}
+
+
+
